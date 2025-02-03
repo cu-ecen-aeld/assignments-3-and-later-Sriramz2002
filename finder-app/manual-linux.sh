117b317bafa3fbf4726d862093a9f9eb42c4a2b9
ubuntu_gui <ubuntu_gui@Ubuntu.myguest.virtualbox.org>
2025-02-02 19:20:49 -0700

    Assignment 3 part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -1,80 +1,142 @@
 #!/bin/bash
-# Script outline to install and build kernel.
-# Author: Siddhant Jajoo.
+# Script to install and build the Linux kernel and root filesystem.
+# Author: Sriramkumar Jayaraman 
 
 set -e
 set -u
 
-OUTDIR=/tmp/aeld
-KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
-KERNEL_VERSION=v5.1.10
+# Define Output Directory
+OUTDIR="${HOME}/lk"
+KERNEL_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
+KERNEL_VERSION=v5.15.163
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
 
-if [ $# -lt 1 ]
-then
-	echo "Using default directory ${OUTDIR} for output"
+# Required Dependencies
+PACKAGES=(
+    flex
+    bison
+    build-essential
+    libssl-dev
+    bc
+    u-boot-tools
+    qemu
+    cpio
+    device-tree-compiler
+)
+
+# Install Missing Dependencies
+sudo apt update && sudo apt install -y "${PACKAGES[@]}"
+
+# Allow Custom Output Directory
+if [ $# -ge 1 ]; then
+    OUTDIR=$1
+    echo "Using provided output directory: ${OUTDIR}"
 else
-	OUTDIR=$1
-	echo "Using passed directory ${OUTDIR} for output"
+    echo "Using default directory: ${OUTDIR}"
 fi
 
-mkdir -p ${OUTDIR}
-
+mkdir -p "$OUTDIR" || { echo "Failed to create ${OUTDIR}"; exit 1; }
 cd "$OUTDIR"
+
+### Clone Linux Kernel ###
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
-    #Clone only if the repository does not exist.
-	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+    echo "Cloning Linux kernel version ${KERNEL_VERSION}"
+    git clone ${KERNEL_REPO} --depth 1 --branch ${KERNEL_VERSION} linux-stable
 fi
-if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
+
+### Build Linux Kernel ###
+if [ ! -e "${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image" ]; then
     cd linux-stable
-    echo "Checking out version ${KERNEL_VERSION}"
-    git checkout ${KERNEL_VERSION}
-
-    # TODO: Add your kernel build steps here
+    echo "Building Kernel................................................"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+    cp arch/${ARCH}/boot/Image ${OUTDIR}/
+    cp -r arch/${ARCH}/boot/dts/ ${OUTDIR}/
 fi
 
-echo "Adding the Image in outdir"
+echo "Kernel Image and Device Tree copied successfully"
 
-echo "Creating the staging directory for the root filesystem"
+### Create Root Filesystem ###
+echo "Setting up root filesystem........................"
 cd "$OUTDIR"
-if [ -d "${OUTDIR}/rootfs" ]
-then
-	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
-fi
+sudo rm -rf ${OUTDIR}/rootfs
+mkdir -p rootfs/{bin,sbin,etc,proc,sys,dev,usr,lib,lib64,var,tmp,home}
+mkdir -p rootfs/usr/{bin,lib,sbin}
+mkdir -p rootfs/var/log
 
-# TODO: Create necessary base directories
-
+### Clone & Build BusyBox ###
 cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/busybox" ]
-then
-git clone git://busybox.net/busybox.git
+if [ ! -d "${OUTDIR}/busybox" ]; then
+    git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+make distclean
+make defconfig
+make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
-echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+# Ensure busybox binary exists
+if [ ! -f "${OUTDIR}/rootfs/bin/busybox" ]; then
+    echo "Error: BusyBox installation failed!"
+    exit 1
+fi
 
-# TODO: Add library dependencies to rootfs
+echo "BusyBox installed successfully."
 
-# TODO: Make device nodes
+### Set Permissions ###
+sudo chmod u+s ${OUTDIR}/rootfs/bin/busybox
 
-# TODO: Clean and build the writer utility
+### Copy Library Dependencies ###
+SYSROOT=$(${CROSS_COMPILE}gcc --print-sysroot)
 
-# TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
+# Ensure both `lib` and `lib64` are copied correctly
+sudo cp -a ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/ || echo "Warning: ld-linux-aarch64.so.1 missing!"
+sudo cp -a ${SYSROOT}/lib64/* ${OUTDIR}/rootfs/lib64/ || echo "Warning: lib64 missing!"
 
-# TODO: Chown the root directory
+# Additional critical libraries for `aarch64`
+sudo cp -a ${SYSROOT}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64/ || echo "Warning: libc.so.6 missing!"
+sudo cp -a ${SYSROOT}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64/ || echo "Warning: libm.so.6 missing!"
+sudo cp -a ${SYSROOT}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64/ || echo "Warning: libresolv.so.2 missing!"
+sudo cp -a ${SYSROOT}/lib64/libdl.so.2 ${OUTDIR}/rootfs/lib64/ || echo "Warning: libdl.so.2 missing!"
 
-# TODO: Create initramfs.cpio.gz
+### Create Device Nodes ###
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
+sudo mknod -m 600 ${OUTDIR}/rootfs/dev/console c 5 1
+
+### Build Finder App (Writer Utility) ###
+cd ${FINDER_APP_DIR}
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
+cp writer ${OUTDIR}/rootfs/home/
+
+### Copy Finder App Scripts ###
+cp ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home/
+cp ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home/
+cp ${FINDER_APP_DIR}/writer ${OUTDIR}/rootfs/home/
+cp ${FINDER_APP_DIR}/autorun-qemu.sh ${OUTDIR}/rootfs/home/
+cp -r ${FINDER_APP_DIR}/conf/ ${OUTDIR}/rootfs/home/
+
+### Fix Finder Test Config Path ###
+sed -i 's|\.\./conf/assignment.txt|conf/assignment.txt|g' ${OUTDIR}/rootfs/home/finder-test.sh
+
+### Set Correct Ownership ###
+sudo chown -R root:root ${OUTDIR}/rootfs/
+
+### Create Initramfs ###
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner=root:root > ${OUTDIR}/initramfs.cpio
+gzip -f ${OUTDIR}/initramfs.cpio
+
+### Final Message ###
+echo " Kernel, root filesystem, and device tree successfully built and ready for QEMU boot!"
+
