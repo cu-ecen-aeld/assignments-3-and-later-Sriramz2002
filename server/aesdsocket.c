1d5e786970ea094493aa012a2afa298963011b4a
ubuntu_gui <ubuntu_gui@Ubuntu.myguest.virtualbox.org>
2025-03-02 13:28:42 -0700

    a6p1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,261 +1,395 @@
+/*******************************************************************************
+ * aesdsocket.c
+ *
+ * A multithreaded socket server using pthreads and a global mutex for
+ * file I/O synchronization. Also includes a periodic timer thread that
+ * writes timestamps every 10 seconds.
+ *
+ * Illustrates safer handling of signals: the signal handler only sets
+ * a global volatile flag. All real cleanup happens outside the handler.
+ ******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <syslog.h>
 #include <signal.h>
-#include <fcntl.h>
 #include <errno.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <syslog.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <sys/queue.h>
+#include <time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#define PORT 9000
+
+#define PORT (9000)
+#define DATAFILE "/var/tmp/aesdsocketdata"
 
 
-#define PATH "/var/tmp/aesdsocketdata"
+static volatile sig_atomic_t g_exit_flag = 0; 
+
+//to protect writes/reads to the /var/tmp/aesdsocketdata file.
+static pthread_mutex_t g_file_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+static pthread_t g_timer_thread; 
+
+
+typedef struct {
+    int listen_fd;
+    FILE *data_fp;      ///var/tmp/aesdsocketdata
+} global_resources_t;
+
+static global_resources_t g_resources = {
+    .listen_fd = -1,
+    .data_fp   = NULL
+};
 
 
 
 
-typedef struct 
-{
 
-    int server_fd;
+typedef struct client_thread_s {
+    pthread_t thread_id;
     int client_fd;
-    FILE *data_file;
-    char *buffer;
-    size_t buffer_size;
-} server_resources_t;
+    struct sockaddr_in client_addr;
+    SLIST_ENTRY(client_thread_s) entries;
+} client_thread_t;
 
-server_resources_t *resources = NULL;
 
-void cleanup(int signum) 
+static SLIST_HEAD(client_list_head, client_thread_s) g_client_list =
+    SLIST_HEAD_INITIALIZER(g_client_list);
+
+
+static void signal_handler(int signo)
 {
-    syslog(LOG_INFO, "Caught signal %d, cleaning up and exiting", signum);
-    if (resources)
+    (void)signo;
+
     
-     {
-        if (resources->client_fd > 0) close(resources->client_fd);
-        if (resources->server_fd > 0) close(resources->server_fd);
-        if (resources->data_file) fclose(resources->data_file);
-        if (resources->buffer) free(resources->buffer);
-        free(resources);
+    g_exit_flag = 1;
+
+
+
+
+  
+    if (g_resources.listen_fd >= 0) 
+    {
+        close(g_resources.listen_fd);
+        g_resources.listen_fd = -1;
     }
-    remove(PATH);
-    closelog();
-    exit(EXIT_SUCCESS);
 }
 
-void setup_signal_handlers() 
+
+static void* timestamp_thread_func(void *arg)
 {
+    (void)arg; 
+
+while (!g_exit_flag) {
+    for (int i = 0; i < 10 && !g_exit_flag; i++) {
+        sleep(1); 
+    }
+
+    if (g_exit_flag) break;  
+
+
+    time_t now = time(NULL);
+    struct tm t;
+    localtime_r(&now, &t);
+
+    char timestamp_str[128];
+    strftime(timestamp_str, sizeof(timestamp_str),
+             "timestamp:%a, %d %b %Y %T %z\n", &t);
+
+
+    pthread_mutex_lock(&g_file_mutex);
+
+    fseek(g_resources.data_fp, 0, SEEK_END);
+    fwrite(timestamp_str, 1, strlen(timestamp_str), g_resources.data_fp);
+    fflush(g_resources.data_fp);
+
+    pthread_mutex_unlock(&g_file_mutex);
+}
+    pthread_exit(NULL);
+}
+
+static void* client_thread_func(void *arg)
+{
+    client_thread_t *client_info = (client_thread_t*)arg;
+
+
+    syslog(LOG_INFO, "Accepted connection from %s",
+           inet_ntoa(client_info->client_addr.sin_addr));
+
+
+    size_t bufsize = 1024;
+    char *recvbuf = malloc(bufsize);
+    if (!recvbuf) {
+        syslog(LOG_ERR, "malloc() failed for client buffer");
+        goto done;
+    }
+    memset(recvbuf, 0, bufsize);
+
+    size_t total_recv = 0;
+    ssize_t rc;
+
+
+    while (!g_exit_flag && (rc = recv(client_info->client_fd, recvbuf + total_recv, bufsize - total_recv - 1, 0)) > 0) 
+    {
+        total_recv += rc;
+        recvbuf[total_recv] = '\0'; 
+
+
+        if (total_recv >= bufsize - 1) {
+            size_t newsize = bufsize * 2;
+            char *tmp = realloc(recvbuf, newsize);
+            if (!tmp) {
+                syslog(LOG_ERR, "realloc failed");
+                goto done;
+            }
+            recvbuf = tmp;
+            bufsize = newsize;
+        }
+
+
+        if (memchr(recvbuf, '\n', total_recv)) {
+            break;
+        }
+    }
+
+
+    if (recvbuf && total_recv > 0) {
+
+        pthread_mutex_lock(&g_file_mutex);
+
+        fseek(g_resources.data_fp, 0, SEEK_END);
+        if (fwrite(recvbuf, 1, total_recv, g_resources.data_fp) != total_recv) {
+            syslog(LOG_ERR, "Error writing data to file");
+        }
+        fflush(g_resources.data_fp);
+
+
+        fseek(g_resources.data_fp, 0, SEEK_SET);
+
+
+        memset(recvbuf, 0, bufsize);
+
+        while (fgets(recvbuf, bufsize, g_resources.data_fp) != NULL) {
+            send(client_info->client_fd, recvbuf, strlen(recvbuf), 0);
+        }
+
+
+        fseek(g_resources.data_fp, 0, SEEK_END);
+
+        pthread_mutex_unlock(&g_file_mutex);
+    }
+
+done:
+
+    syslog(LOG_INFO, "Closing connection from %s",
+           inet_ntoa(client_info->client_addr.sin_addr));
+
+
+    if (client_info->client_fd >= 0) {
+        close(client_info->client_fd);
+        client_info->client_fd = -1;
+    }
+    if (recvbuf) {
+        free(recvbuf);
+    }
+    pthread_exit(NULL);
+}
+
+
+int main(int argc, char *argv[])
+{
+    int daemon_mode = 0;
+    if ((argc == 2) && (strcmp(argv[1], "-d") == 0)) {
+        daemon_mode = 1;
+    }
+
+
+    openlog("aesdsocket", LOG_PID, LOG_USER);
+
+
     struct sigaction sa;
     memset(&sa, 0, sizeof(sa));
-    
-    sa.sa_handler = cleanup;
+    sa.sa_handler = signal_handler;
     sigaction(SIGINT, &sa, NULL);
-    
     sigaction(SIGTERM, &sa, NULL);
-}
 
-void daemonize() 
-{
-    pid_t pid = fork();
 
-    if (pid < 0) 
-    {
-        syslog(LOG_ERR, "fork failed: %s", strerror(errno));
-        exit(EXIT_FAILURE);
+    g_resources.listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (g_resources.listen_fd < 0) {
+        syslog(LOG_ERR, "socket() failed: %s", strerror(errno));
+        return EXIT_FAILURE;
     }
 
-    if (pid > 0) 
-    {
-        // Parent exits
-        exit(EXIT_SUCCESS); 
-    }
+    int optval = 1;
+    setsockopt(g_resources.listen_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
 
-    umask(0);
+    struct sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
 
-    if (setsid() < 0)
-    {
-        syslog(LOG_ERR, "failed to create new session: %s", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-
-    // Redirect standard files to /dev/null
-    freopen("/dev/null", "r", stdin);
-    freopen("/dev/null", "w", stdout);
-    freopen("/dev/null", "w", stderr);
-
-    if (chdir("/") < 0) 
-    {
-        syslog(LOG_ERR, "failed to change directory: %s", strerror(errno));
-        exit(EXIT_FAILURE);
+    if (bind(g_resources.listen_fd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        syslog(LOG_ERR, "bind() failed: %s", strerror(errno));
+        close(g_resources.listen_fd);
+        return EXIT_FAILURE;
     }
 
 
-    FILE *pid_file = fopen("/var/run/aesdsocket.pid", "w");
-    if (pid_file) {
-        fprintf(pid_file, "%d\n", getpid());
-        fclose(pid_file);
-    } else {
-        syslog(LOG_ERR, "failed to write PID file: %s", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-}
+    if (daemon_mode) {
+        pid_t pid = fork();
+        if (pid < 0) {
+            syslog(LOG_ERR, "fork failed: %s", strerror(errno));
+            close(g_resources.listen_fd);
+            return EXIT_FAILURE;
+        } else if (pid > 0) {
 
-int main(int argc, char *argv[]) 
-{
-    struct sockaddr_in address, client_addr ;
-    
-    socklen_t client_addr_len = sizeof(client_addr );
-    ssize_t bytes_read;
-    int daemon= 0  ;
+            return EXIT_SUCCESS;
+        }
 
-    openlog("aesdsocket", LOG_PID | LOG_CONS, LOG_USER );
+        umask(0);
+        if (setsid() < 0) {
+            syslog(LOG_ERR, "setsid failed: %s", strerror(errno));
+            close(g_resources.listen_fd);
+            return EXIT_FAILURE;
+        }
 
-
-    if (argc == 2 && strcmp(argv[1], "-d") == 0  ) 
-    {
-        daemon= 1 ;
-    }
-
-
-    setup_signal_handlers();
-
-    resources = calloc(1, sizeof(server_resources_t));
-    if (!resources) {
-        syslog(LOG_ERR, "failed to allocate memory for resources");
-        exit(EXIT_FAILURE);
-    }
-
-
-    resources->buffer_size = 1000;
-    resources->buffer = malloc(resources->buffer_size);
-    if (!resources->buffer) 
-    {
-        syslog(LOG_ERR, "failed to allocate initial buffer memory");
-        cleanup(SIGTERM);
-    }
-
-    // Create the socket
-    resources->server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    
-    if (resources->server_fd == -1) 
-    {
-        syslog(LOG_ERR, "failed to create socket: %s", strerror(errno));
-        cleanup(SIGTERM);
-    }
-
-    // Allow reuse of the port
-    int opt = 1;
-    if (setsockopt(resources->server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
-        syslog(LOG_ERR, "setsockopt failed: %s", strerror(errno));
-        cleanup(SIGTERM);
-    }
-
-    // Bind the socket
-    memset(&address, 0, sizeof(address));
-    
-    address.sin_family = AF_INET;
-    
-    address.sin_addr.s_addr = INADDR_ANY;
-    
-    address.sin_port = htons(PORT);
-
-    if (bind(resources->server_fd, (struct sockaddr *)&address, sizeof(address)) == -1) 
-    {
-        syslog(LOG_ERR, "Bind failed: %s", strerror(errno));
+        freopen("/dev/null", "r", stdin);
         
-        cleanup(SIGTERM);
+        freopen("/dev/null", "w", stdout);
+        
+        freopen("/dev/null", "w", stderr);
+
+
+        FILE *pidfp = fopen("/var/run/aesdsocket.pid", "w");
+        if (pidfp) {
+            fprintf(pidfp, "%d\n", getpid());
+            fclose(pidfp);
+        } else {
+            syslog(LOG_ERR, "could not open /var/run/aesdsocket.pid for writing");
+        }
+    }
+
+    
+    if (listen(g_resources.listen_fd, 10) < 0) {
+        syslog(LOG_ERR, "listen() failed: %s", strerror(errno));
+        close(g_resources.listen_fd);
+        return EXIT_FAILURE;
     }
 
 
-    if (daemon) 
-    {
-        daemonize();
+    g_resources.data_fp = fopen(DATAFILE, "a+");
+    if (!g_resources.data_fp) {
+        syslog(LOG_ERR, "fopen(%s) failed: %s", DATAFILE, strerror(errno));
+        close(g_resources.listen_fd);
+        return EXIT_FAILURE;
     }
 
 
-    if (listen(resources->server_fd, 10) == -1) 
-    {   syslog(LOG_ERR, "listen failed: %s", strerror(errno));
-        cleanup(SIGTERM);
+    if (pthread_create(&g_timer_thread, NULL, timestamp_thread_func, NULL) != 0) {
+        syslog(LOG_ERR, "pthread_create for timer thread failed");
+
     }
 
+    syslog(LOG_INFO, "aesdsocket started, listening on port %d", PORT);
 
-    resources->data_file = fopen(PATH, "a+");
-    if (!resources->data_file) {
-        syslog(LOG_ERR, "failed to open file: %s", strerror(errno));
-        cleanup(SIGTERM);
-    }
 
-    // Main loop for accepting connections
-    while (1) 
-    {
-        resources->client_fd = accept(resources->server_fd , (struct sockaddr *)&client_addr, &client_addr_len);
-        if (resources->client_fd == -1) {
-            syslog(LOG_ERR, "Accept failed: %s", strerror(errno));
+    while (!g_exit_flag) {
+        struct sockaddr_in clientaddr;
+        socklen_t addrlen = sizeof(clientaddr);
+
+        int client_fd = accept(g_resources.listen_fd,
+                               (struct sockaddr*)&clientaddr,
+                               &addrlen);
+        if (g_exit_flag) 
+        {
+
+            break;
+        }
+        if (client_fd < 0) {
+
+            if (errno == EINTR) continue;
+            
+            syslog(LOG_ERR, "accept failed: %s", strerror(errno));
+            break;
+        }
+
+
+        client_thread_t *ct = calloc(1, sizeof(client_thread_t));
+        if (!ct) {
+            syslog(LOG_ERR, "calloc() for client_thread_t failed");
+            close(client_fd);
             continue;
         }
-
-        syslog(LOG_INFO, "connection accepted:- %s", inet_ntoa(client_addr.sin_addr));
-
-        size_t total_received = 0;
-        ssize_t received;
+        ct->client_fd = client_fd;
+        ct->client_addr = clientaddr;
 
 
-        memset(resources->buffer, 0, resources->buffer_size);
+        SLIST_INSERT_HEAD(&g_client_list, ct, entries);
 
 
-        while ((received = recv(resources->client_fd , resources->buffer + total_received  , resources->buffer_size - total_received - 1  , 0) ) >  0) 
-        {
-            total_received += received;
-
-
-            if (total_received >= resources->buffer_size - 1) 
-            {
+        if (pthread_create(&ct->thread_id, NULL, client_thread_func, ct) != 0) {
+            syslog(LOG_ERR, "pthread_create for client failed");
             
-                size_t new_size = resources->buffer_size * 2;
-                char *new_buffer = realloc(resources->buffer, new_size);
-                if (!new_buffer) {
-                    syslog(LOG_ERR, "failed_realloc ");
-                    cleanup(SIGTERM);
-                }
-                resources->buffer = new_buffer;
-                resources->buffer_size = new_size;
-            }
+            close(client_fd);
 
-
-            if (memchr(resources->buffer, '\n', total_received)) {
-                break;
-            }
+            SLIST_REMOVE(&g_client_list, ct, client_thread_s, entries);
+            free(ct);
         }
-
-        if (received == -1) {
-            syslog(LOG_ERR, "receive failed: %s", strerror(errno));
-        } else if (total_received > 0) {
-
-            if (fwrite(resources->buffer, 1, total_received, resources->data_file) != total_received) {
-                syslog(LOG_ERR, "write to file failed: %s", strerror(errno));
-            }
-            fflush(resources->data_file);
-
-
-            fseek(resources->data_file, 0, SEEK_SET);
-            while (fgets(resources->buffer, resources->buffer_size, resources->data_file)) {
-                send(resources->client_fd, resources->buffer, strlen(resources->buffer), 0);
-            }
-            fseek(resources->data_file, 0, SEEK_END);
-        }
-
-        syslog(LOG_INFO, "connection clsed:- %s", inet_ntoa(client_addr.sin_addr));
-        close(resources->client_fd);
-        resources->client_fd = -1;
     }
 
-    cleanup(SIGTERM);
+
+
+
+    syslog(LOG_INFO, "shutting down aesdsocket...........................................");
+
+    if (g_resources.listen_fd >= 0)
+     {
+        close(g_resources.listen_fd);
+        g_resources.listen_fd = -1;
+    }
+
+
+    client_thread_t *it = NULL;
+    client_thread_t *tmp = NULL;
+
+
+
+    for (it = SLIST_FIRST(&g_client_list); it != NULL; it = tmp) 
+    {
+        tmp = SLIST_NEXT(it, entries);
+
+
+        pthread_join(it->thread_id, NULL);
+
+
+        SLIST_REMOVE(&g_client_list, it, client_thread_s, entries);
+        if (it->client_fd >= 0) close(it->client_fd);
+        free(it);
+    }
+
+
+    pthread_join(g_timer_thread, NULL);
+
+
+    if (g_resources.data_fp) {
+        fclose(g_resources.data_fp);
+        g_resources.data_fp = NULL;
+    }
+
+
+    remove(DATAFILE);
+
+    syslog(LOG_INFO, "aesdsocket clean exit");
+    closelog();
+
     return 0;
 }
 
